13c766f18be3739d3576aa58644c88c85edd6b02
hyeols <celinelove23@naver.com>
2024-08-23 18:31:51 +0900

    add spring boot

---
+++ betta-service/src/main/java/com/betta/service/betta_service/BettaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.betta.service.betta_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BettaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BettaServiceApplication.class, args);
+	}
+
+}
---
+++ betta-service/src/test/java/com/betta/service/betta_service/BettaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betta.service.betta_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BettaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
